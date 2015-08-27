1b05183816682f39109212580ddc8fe4ee82182b
Cristina <cristina.colon+git@gmail.com>
2015-08-27 18:36:54 -0400

    wrongIndex nonfunctionality complete... or is it

---
+++ index.js
@@ -0,0 +1,23 @@
+Array.prototype.wrongIndexOf = function (value) {
+	function randomIndex (arrayLength) {
+		return Math.floor(Math.random() * arrayLength);
+	}
+	var realIndex = this.indexOf(value);
+	var someIndex = randomIndex(this.length);
+	if (realIndex===-1) {return someIndex;}
+	while (someIndex===realIndex){
+		someIndex = randomIndex(this.length);
+	}
+	return someIndex;
+};
+
+/*
+TO DO
+-Object.syek()
+-stackoverflow
+-hasPrototype
+-Object.notAFunction
+-dishonest
+-throwThisAway
+-isThisUseful
+*/
